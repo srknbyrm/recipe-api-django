6ef194ba0036ad92e754b555a35bb77fe22a70cc
srknbyrm <bayram.serkan@outlook.com>
2022-08-08 17:33:03 +0300

    Custom user authentication with token

---
+++ application/user/urls.py
@@ -0,0 +1,16 @@
+"""
+    Url mappings for user api
+"""
+
+from django.urls import path
+from user import views
+
+app_name = "user"
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
+    # reverse (user:create) that why it should be same naming
+
+]
